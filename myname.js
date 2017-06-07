c7d3ca33ec0eca6995c2307de27b071a58c2bc43
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-06-07 10:49:12 +0200

    b6

--- myname.js
+++ myname.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let Greet = function(firstName, lastName) {
+let Greet = function(firstName, lastName, parameter) {
     console.log('Hello ' + firstName + ' ' + lastName + 'noob nolifer nerd');
+    console.log('Greating for the ' + parameter + '!');
 }
 
-Greet('Balázs', 'Ilcsik');
+Greet('Balázs', 'Ilcsik', 'Me');
