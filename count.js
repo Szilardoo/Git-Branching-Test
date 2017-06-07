b73a9cc35057c41bc566f1f89ca8d717899c55e3
Simon_NT <kacsa1984@gmail.com>
2017-06-07 10:49:45 +0200

    c6

--- count.js
+++ count.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function count(age, month, bornYear){
+function count(age, month, bornYear, infinite){
 	console.log(age + ' ' + month + ' ' + bornYear);
+    console.log('Counting the countless ' + infinite);
 }
 
 count(20, 'july',1996);
