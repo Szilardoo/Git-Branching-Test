afe0190e62d4bc57ca55097c8a371511824325c7
Simon_NT <kacsa1984@gmail.com>
2017-06-07 10:31:00 +0200

    c3

--- myname.js
+++ myname.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let Greet = function(firstName, lastName) {
-    console.log('Hello ' + firstName + ' ' + lastName);
+    console.log('Hello ' + firstName + ' ' + lastName + 'noob nolifer nerd');
 }
 
 Greet('Balázs', 'Ilcsik');
