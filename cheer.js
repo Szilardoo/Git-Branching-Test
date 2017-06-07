02fe311126a04f16e3370a081b7398fb01a7ec5b
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-06-07 10:48:22 +0200

    a5

--- cheer.js
+++ cheer.js
@@ -1,7 +1,7 @@
-let cheer = function(name){
+let cheer = function(name, lol){
     console.log(name + 'must DIIIIIEEEEEEEEE!!!')
     console.log('The Order of the Phoenix will RIIIIIIISSSSSSEEEEEEEE');
-    console.log('Chinese');
+    console.log('Chinese', lol);
 };
 
-cheer('Voldemort');
+cheer('Voldemort', 'kek');
